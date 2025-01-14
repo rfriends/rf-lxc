fb5764d99d544feddf578356c36e48e16c55c7ff
mapi <rfriends2017@ymail.ne.jp>
2025-01-15 02:52:45 +0900

    Update and rename rfriends_lxd.sh to rfriends_install.sh

--- rfriends_install.sh
+++ rfriends_install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #
 cd ~/  
-rm -rf rfriends3_ubuntu
+rm -rf rfriends_ubuntu
 sudo apt-get install git -y  
 git clone https://github.com/rfriends/rfriends_ubuntu.git
 cd rfriends_ubuntu  
 sh rfriends_ubuntu.sh  
  
-hostname -i  
+hostname -I  
 echo
 echo ホストのWebブラウザで、IPアドレス:8000 でアクセスしてください
 exit 0
